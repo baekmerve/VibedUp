a97edd309c476171401a3001c35800f9396a0a73
baekmerve <baekmerve@gmail.com>
2025-01-04 12:07:48 +0900

    moved update function to parent component

--- app/components/EditModal.jsx
+++ app/components/EditModal.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Modal, Text, TextInput, TouchableOpacity, View } from "react-native";
 import { useGlobalContext } from "../../context/GlobalProvider";
 
@@ -8,20 +8,16 @@ const EditModal = ({
   onClose,
   initialData,
   type,
+  handleUpdate,
 }) => {
   const [title, setTitle] = useState(initialData.title);
   const [content, setContent] = useState(initialData.content);
-  const { updateContent } = useGlobalContext(); // Get updateContent from context
 
-  const handleSave = async () => {
-    try {
-      await updateContent(contentId, { title, content }, type); // Use the update function from context
-      alert(`${type} updated successfully`);
-      onClose(); // Close the modal after successful update
-    } catch (error) {
-      alert(`Error updating content: ${error.message}`);
-    }
-  };
+  // Synchronize state with initialData when it changes
+  useEffect(() => {
+    setTitle(initialData.title);
+    setContent(initialData.content);
+  }, [initialData]);
 
   return (
     <Modal visible={visible} animationType="slide" transparent={true}>
@@ -30,20 +26,21 @@ const EditModal = ({
         activeOpacity={1}
         onPress={onClose}
       >
-        <View className="bg-white  p-6 rounded-lg w-4/5 max-w-lg">
+        <View className="bg-white border border-gray-400 p-6 rounded-2xl shadow-2xl w-4/5 max-w-lg ">
           <Text className=" text-brown text-lg font-semibold mb-4 text-center">
             Edit {type}
           </Text>
 
           <TextInput
-            className=" text-brown border border-gray-300 rounded-lg p-3 mb-4 text-sm "
+            className=" text-brown border border-gray-400 rounded-lg p-3 mb-4 text-sm "
             value={title}
             onChangeText={setTitle}
             placeholder="Edit Title"
             placeholderTextColor="#888"
+            multiline={true}
           />
           <TextInput
-            className="text-brown border border-gray-300 rounded-lg p-3 mb-4 h-32 text-sm"
+            className="text-brown border border-gray-400 rounded-lg p-3 mb-4 h-44 text-sm"
             value={content}
             onChangeText={setContent}
             placeholder="Edit Content"
@@ -60,8 +57,8 @@ const EditModal = ({
             </TouchableOpacity>
 
             <TouchableOpacity
-              className="bg-blue-500 p-3 rounded-lg w-1/3"
-              onPress={handleSave}
+              className="bg-blueGreen p-3 rounded-lg w-1/3"
+              onPress={() => handleUpdate(contentId, { title, content }, type)}
             >
               <Text className="text-center text-sm text-white">Save</Text>
             </TouchableOpacity>
