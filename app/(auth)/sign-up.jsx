d1088f668566f0ab1c36cf3d5738747da26f232c
baekmerve <baekmerve@gmail.com>
2025-01-07 23:02:53 +0900

    added Validation and adding profile picture function

--- app/(auth)/sign-up.jsx
+++ app/(auth)/sign-up.jsx
@@ -1,4 +1,13 @@
-import { View, Text, ScrollView, Image, Alert } from "react-native";
+import {
+  View,
+  Text,
+  ScrollView,
+  Image,
+  Alert,
+  TouchableOpacity,
+  KeyboardAvoidingView,
+  Platform,
+} from "react-native";
 import React, { useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { images } from "../../constants";
@@ -7,21 +16,51 @@ import CustomButton from "../components/CustomButton";
 import { Link, router } from "expo-router";
 import { createAccount } from "../../lib/appwrite";
 import { useGlobalContext } from "../../context/GlobalProvider";
+import { icons } from "../../constants";
+import * as ImagePicker from "expo-image-picker";
 
 const SignUp = () => {
   const { setUser, setIsLoggedIn } = useGlobalContext();
+  const [isSubmitting, setIsSubmitting] = useState(false);
 
   const [form, setForm] = useState({
     username: "",
     email: "",
     password: "",
+    avatar: null,
   });
 
-  const [isSubmitting, setIsSubmitting] = useState(false);
+  const openPicker = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ["images"],
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    if (!result.canceled) {
+      setForm({
+        ...form,
+        avatar: result.assets[0],
+      });
+    }
+  };
 
   const handleSubmit = async () => {
     if (!form.username || !form.password || !form.email) {
       Alert.alert("Error", "Please fill in all the fields");
+    }
+
+    // Simple Email Validation
+    const emailRegex = /\S+@\S+\.\S+/;
+    if (!emailRegex.test(form.email)) {
+      Alert.alert("Error", "Please enter a valid email address");
+      return;
+    }
+
+    // Password strength check 
+    if (form.password.length < 8) {
+      Alert.alert("Error", "Password must be at least 8 characters");
+      return;
     }
     setIsSubmitting(true);
 
@@ -29,7 +68,8 @@ const SignUp = () => {
       const result = await createAccount(
         form.email,
         form.password,
-        form.username
+        form.username,
+        form.avatar
       );
 
       setUser(result);
@@ -45,57 +85,89 @@ const SignUp = () => {
 
   return (
     <SafeAreaView className="bg-paper h-full">
-      <ScrollView>
-        <View className="w-full h-full justify-center px-4 my-6">
-          <Image
-            source={images.logo}
-            resizeMode="contain"
-            className="w-[160px] h-[90px]"
-          />
-          <Text className="font-psemibold text-2xl text-brown mt-10 text-semibold ">
-            Sign up to VibedUp!
-          </Text>
-          <FormField
-            title="Username"
-            placeholder="Enter a unique username"
-            value={form.username}
-            handleChangeText={(e) => setForm({ ...form, username: e })}
-            otherStyles="mt-10"
-          />
-          <FormField
-            title="Email"
-            placeholder="Enter your email address"
-            value={form.email}
-            handleChangeText={(e) => setForm({ ...form, email: e })}
-            otherStyles="mt-7"
-            keybordType="email-address"
-          />
-          <FormField
-            title="Password"
-            placeholder="Enter a password contains of 8 character"
-            value={form.password}
-            handleChangeText={(e) => setForm({ ...form, password: e })}
-            otherStyles="mt-7"
-          />
-          <CustomButton
-            title="Sign-In"
-            handlePress={handleSubmit}
-            containerStyles="mt-7"
-            isLoading={isSubmitting}
-          />
-          <View className="flex-row justify-center pt-5 gap-2">
-            <Text className="text-lg text-brown font-pregular">
-              Already have an account?
+      {/* for smooth keyboard interaction. */}
+      <KeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+        style={{ flex: 1 }}
+      >
+        <ScrollView>
+          <View className="w-full h-full justify-center px-4 my-3">
+            <Image
+              source={images.logo}
+              resizeMode="contain"
+              className="w-[160px] h-[90px]"
+            />
+            <Text className="font-psemibold text-2xl text-brown  text-semibold ">
+              Sign up to VibedUp!
             </Text>
-            <Link
-              href="/sign-in"
-              className="text-lg font-psemibold text-secondary"
+            <TouchableOpacity
+              onPress={() => openPicker("image")}
+              className="mt-3 items-center"
             >
-              Login
-            </Link>
+              {form.avatar ? (
+                <Image
+                  source={{ uri: form.avatar.uri }}
+                  className="w-32 h-32 rounded-[50%]"
+                  resizeMode="cover"
+                />
+              ) : (
+                <View className=" justify-center items-center flex-row mt-1">
+                  <View className="items-center">
+                    <Image
+                      source={icons.upload2}
+                      resizeMode="contain"
+                      className="w-14 h-14 "
+                    />
+                    <Text className="text-xs mt-2 text-brown font-pmedium">
+                      upload a picture
+                    </Text>
+                  </View>
+                </View>
+              )}
+            </TouchableOpacity>
+            <FormField
+              title="Username"
+              placeholder="Enter a unique username"
+              value={form.username}
+              handleChangeText={(e) => setForm({ ...form, username: e })}
+              otherStyles="mt-5"
+            />
+            <FormField
+              title="Email"
+              placeholder="Enter your email address"
+              value={form.email}
+              handleChangeText={(e) => setForm({ ...form, email: e })}
+              otherStyles="mt-5"
+              keybordType="email-address"
+            />
+            <FormField
+              title="Password"
+              placeholder="Enter a password contains of 8 character"
+              value={form.password}
+              handleChangeText={(e) => setForm({ ...form, password: e })}
+              otherStyles="mt-5"
+            />
+
+            <CustomButton
+              title="Sign-In"
+              handlePress={handleSubmit}
+              containerStyles="mt-10"
+              isLoading={isSubmitting}
+            />
+            <View className="flex-row justify-center pt-5 gap-2">
+              <Text className="text-lg text-brown font-pregular">
+                Already have an account?
+              </Text>
+              <Link
+                href="/sign-in"
+                className="text-lg font-psemibold text-secondary"
+              >
+                Login
+              </Link>
+            </View>
           </View>
-        </View>
-      </ScrollView>
+        </ScrollView>
+      </KeyboardAvoidingView>
     </SafeAreaView>
   );
 };
