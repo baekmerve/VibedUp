4498042482f7572f4215051948363bac8b6fa210
baekmerve <baekmerve@gmail.com>
2025-01-10 22:52:30 +0900

    combined post/video functions, had a simplied version of the functions

--- context/GlobalProvider.js
+++ context/GlobalProvider.js
@@ -4,14 +4,12 @@ import {
   fetchAllVideos,
   fetchAllPosts,
   fetchLatestVideos,
-  getUserLikedVideos,
-  getUserLikedPosts,
+  getUserSavedVideos,
+  getUserSavedPosts,
   deleteUserVideo,
   deleteUserPost,
-  likeVideo,
-  likePost,
-  unlikeVideo,
-  unlikePost,
+  saveVideo,
+  savePost,
   getUserVideos,
   getUserPosts,
   editUserVideo,
@@ -33,36 +31,36 @@ const GlobalProvider = ({ children }) => {
   const [user, setUser] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
 
-  const [videos, setVideos] = useState([]);
-  const [posts, setPosts] = useState([]);
-
   const [latestPosts, setLatestPosts] = useState([]);
-  const [userVideos, setUserVideos] = useState([]);
-  const [userPosts, setUserPosts] = useState([]);
 
-  const [likedVideoList, setLikedVideoList] = useState([]);
-  const [savedVideoId, setSavedVideoId] = useState([]);
-  const [likedPostList, setLikedPostList] = useState([]);
-  const [savedPostId, setSavedPostId] = useState([]);
+  const [allContents, setAllContents] = useState([]);
+  const [userContents, setUserContents] = useState([]);
+  const [savedContents, setSavedContents] = useState([]);
+  const [savedContentId, setSavedContentId] = useState([]);
 
   const [searchPostResult, setSearchPostResult] = useState([]);
   const [searchVideoResult, setSearchVideoResult] = useState([]);
 
   const [refreshing, setRefreshing] = useState(false);
-  const [menuState, setMenuState] = useState({});
 
   // function for fetchUserData
 
+  // place fetchUserData in context to avoid redundancy
   const fetchUserData = async () => {
     try {
-      const userInfo = await getCurrentUser();
-      if (userInfo) {
+      const currentUser = await getCurrentUser();
+      if (currentUser) {
         setIsLoggedIn(true);
-        setUser(userInfo);
-        await Promise.all([fetchUserLikedVideos(), fetchUserLikedPosts()]);
+        setUser(currentUser);
+
+        const filteredUserPosts = allContents.filter(
+          (content) => content.creator.$id === user.$id
+        );
+        setUserContents(filteredUserPosts);
       } else {
         setIsLoggedIn(false);
         setUser(null);
+        setUserContents([]);
       }
     } catch (error) {
       console.error("Error fetching current user:", error);
@@ -70,177 +68,181 @@ const GlobalProvider = ({ children }) => {
       setIsLoading(false);
     }
   };
+
   useEffect(() => {
-    fetchUserData();
+    fetchUserData(); // Fetch data when the app starts
   }, []);
 
-  // function for commonRefresh
-  const commonRefresh = async (fetchData) => {
-    if (refreshing) return; // Prevent refreshing if it's already in progress
-
-    setRefreshing(true); // Start refreshing
-    try {
-      // Call the fetch function passed as argument
-      await fetchData();
-    } catch (error) {
-      console.error("Error refreshing data:", error);
-    } finally {
-      setRefreshing(false); // Stop refreshing when done
-    }
-  };
-  // function for open/close the menu
-  const handleToggleMenu = (itemId) => {
-    setMenuState((prev) => ({
-      ...prev,
-      [itemId]: !prev[itemId],
-    }));
-  };
-
   // function for handleCreatePost
-  const handleCreatePost = async (newPostData) => {
+  const handleCreatePost = async (newData) => {
     try {
-      // Assuming createPost is the function that adds a new post to the database
-      await createPost(newPostData);
+      const result = await createPost(newData);
 
-      // Refetch the user posts after creating a new post
-      await fetchUserPostList(user.$id); // You can use the function that fetches the posts for the profile
-      await fetchAllPostContent();
+      setAllContents((prevContents) => {
+        const updatedContents = [result, ...prevContents];
+        console.log("Updated allContents", updatedContents);
+        return updatedContents;
+      });
+       fetchAllContents();
     } catch (error) {
       console.error("Error creating post:", error);
     }
   };
 
   // function for handleCreatVideo
-  const handleCreatVideo = async (newPostData) => {
+  const handleCreatVideo = async (newData) => {
     try {
-      // Assuming createPost is the function that adds a new post to the database
-      await createVideo(newPostData);
+      const result = await createVideo(newData);
 
-      // Refetch the user posts after creating a new post
-      //setPosts((prevPosts) => [...prevPosts, newPostData]);
-      await fetchUserVideoList(user.$id); // You can use the function that fetches the posts for the profile
-      await fetchAllVideoContent();
+      setAllContents((prevContents) => {
+        const updatedContents = [result, ...prevContents];
+        return updatedContents;
+      });
+      fetchAllContents();
     } catch (error) {
       console.error("Error creating post:", error);
     }
   };
-
-  // function for Like a Video
-  const toggleLikeVideo = async (videoId) => {
+  // function for toggleSaveContent
+  const toggleSaveContent = async (contentId, type) => {
     if (!user) return; // Ensure user is logged in
     try {
-      if (savedVideoId.includes(videoId)) {
-        // Video is already liked, so unlike it
-        await unlikeVideo(user.$id, videoId);
-        // Remove the video ID
-        setSavedVideoId((prev) => prev.filter((id) => id !== videoId));
-      } else {
-        // Video is not liked, so like it
-        await likeVideo(user.$id, videoId);
-        // Add the video ID
-        setSavedVideoId((prev) => [...prev, videoId]);
-      }
+      // Dynamically get the current liked state (video or post)
+      const saveFunction = type === "video" ? saveVideo : savePost;
 
-      // After like/unlike, refetch the data to show the updated list
-      fetchUserData(); // Refetch liked posts and videos
+      await saveFunction(user.$id, contentId); // Like the content
+      console.log("saved", contentId);
+
+      setSavedContentId((prev) => [contentId, ...prev]); //
+
+      fetchUserSavedContent(user.$id);
     } catch (error) {
-      console.error("Error toggling like:", error);
+      console.error("Error toggling save/remove:", error);
     }
   };
 
-  //function for Like a Posts
-  const toggleLikePost = async (postId) => {
-    if (!user) return; // Ensure user is logged in
+  // function for fetchUserSavedContent
 
+  const fetchUserSavedContent = async (userId, type = null) => {
+    if (!user) {
+      console.error("User is not logged in");
+      return [];
+    }
     try {
-      if (savedPostId.includes(postId)) {
-        await unlikePost(user.$id, postId); // Call `unlikePost`
-        setSavedPostId((prev) => prev.filter((id) => id !== postId)); // Remove the video ID
-      } else {
-        await likePost(user.$id, postId); // Call `likePost`
-        setSavedPostId((prev) => [...prev, postId]); // Add the video ID
+      // Initialize arrays for liked videos and posts
+      let savedVideos = [];
+      let savedPosts = [];
+      // Fetch video content
+      if (!type || type === "video") {
+        savedVideos = await getUserSavedVideos(userId);
       }
-      const updatedLikedPosts = await fetchUserLikedPosts();
-      setLikedPostList(updatedLikedPosts);
+      // Fetch post content
+      if (!type || type === "post") {
+        savedPosts = await getUserSavedPosts(userId);
+      }
+      // Combine and return content
+      const savedContentList = [
+        ...savedVideos.map((item) => ({ ...item, type: "video" })),
+        ...savedPosts.map((item) => ({ ...item, type: "post" })),
+      ].sort((a, b) => new Date(b.$updatedAt) - new Date(a.$updatedAt));
 
-      // After like/unlike, refetch the data to show the updated list
-      fetchUserData(); // Refetch liked posts and videos
+      setSavedContentId(savedContentList.map((content) => content.$id));
+
+      // Set the combined content to state
+      setSavedContents(savedContentList);
     } catch (error) {
-      console.error("Error toggling like:", error);
+      console.error(
+        "Error fetching user saved content:",
+        error.response || error.message
+      );
+      throw new Error(
+        error.message || "Failed to fetch user saved content list"
+      );
     }
   };
 
   //function for updateContent
-  const updateContent = async (contentId, updatedData, type) => {
+  const updateContent = async (contentId, updatedData, type, updateState) => {
     if (!user) return;
     try {
       // Perform the update for either video or post
       if (type === "video") {
         await editUserVideo(contentId, updatedData);
-        setVideos((prevVideos) =>
+        setUserContents((prevVideos) =>
           prevVideos.map((video) =>
             video.$id === contentId ? { ...video, ...updatedData } : video
           )
         );
       } else if (type === "post") {
         await editUserPost(contentId, updatedData);
-        setPosts((prevPosts) =>
+        setUserContents((prevPosts) =>
           prevPosts.map((post) =>
-            post.$id === contentId ? { ...post, ...updatedData } : post
-          )
-        );
-
-        // Update user posts list specifically for profile page
-        setUserPosts((prevUserPosts) =>
-          prevUserPosts.map((post) =>
             post.$id === contentId ? { ...post, ...updatedData } : post
           )
         );
       }
 
-      return `${type} updated successfully`;
+      updateState((prevContent) =>
+        prevContent.map((post) =>
+          post.$id === contentId ? { ...post, ...updatedData } : post
+        )
+      );
     } catch (error) {
       console.error("Error updating content:", error.message);
       throw new Error(error.message || "Failed to update content");
     }
   };
 
-  //function for fetch all videos
-  const fetchAllVideoContent = async () => {
+  //function for fetchAllContents {
+  const fetchAllContents = async (type = null) => {
     try {
-      const videoList = await fetchAllVideos();
-      setVideos(videoList);
+      const fetchContentByType = async (fetchFunction, contentType) => {
+        const content = await fetchFunction();
+        console.log(`Fetched ${content.length} ${contentType}(s)`);
+        return content.map((item) => ({ ...item, type: contentType }));
+      };
+
+      // Fetch content based on the type
+      const videos =
+        !type || type === "video"
+          ? await fetchContentByType(fetchAllVideos, "video")
+          : [];
+
+      const posts =
+        !type || type === "post"
+          ? await fetchContentByType(fetchAllPosts, "post")
+          : [];
+
+      // Combine and sort content
+      const combinedContent = [...videos, ...posts].sort(
+        (a, b) => new Date(b.$createdAt) - new Date(a.$createdAt)
+      );
+      setAllContents(combinedContent);
+
+      return combinedContent;
     } catch (error) {
-      console.error("Error fetchAllContent:", error.message);
-      throw new Error(error.message || "Failed to fetch video content");
+      const errorDetails =
+        error.response || error.message || JSON.stringify(error);
+      console.error("Error fetching all content:", errorDetails);
+      throw new Error("Failed to fetch all content list");
     }
   };
 
-  //function for fetch all posts
-  const fetchAllPostContent = async () => {
-    try {
-      const postList = await fetchAllPosts();
-      setPosts(postList);
-    } catch (error) {
-      console.error("Error fetchAllPostContent:", error.message);
-      throw new Error(error.message || "Failed to fetch post content");
-    }
-  };
-
-  //function for fetch all posts
+  //function for fetch fetchLatestVideoContent
   const fetchLatestVideoContent = async () => {
     try {
       const response = await fetchLatestVideos();
       setLatestPosts(response);
     } catch (error) {
-      console.error("Error fetchAllPostContent:", error.message);
+      console.error("Error fetchLatestVideoContent:", error.message);
       throw new Error(error.message || "Failed to fetch post content");
     }
   };
 
   //function for deleteContent
-  const deleteContent = async (contentId, type, refetchFunction) => {
+  const deleteContent = async (contentId, type, updateState) => {
     if (!user) return;
+
     try {
       Alert.alert(
         "Delete Content",
@@ -251,53 +253,71 @@ const GlobalProvider = ({ children }) => {
             text: "Delete",
             style: "destructive",
             onPress: async () => {
-              if (type === "video") {
-                await deleteUserVideo(user.$id, contentId); // Call delete API for video
-                setVideos((prevVideos) =>
-                  prevVideos.filter((video) => video.$id !== contentId)
+              console.log(`Deleting ${type} with ID: ${contentId}`);
+              try {
+                if (type === "video") {
+                  await deleteUserVideo(user.$id, contentId); // Delete video API call
+                } else if (type === "post") {
+                  await deleteUserPost(user.$id, contentId); // Delete post API call
+                }
+
+                // Update state to reflect the deletion
+                updateState((prevContent) =>
+                  prevContent.filter((item) => item.$id !== contentId)
                 );
-              } else if (type === "post") {
-                await deleteUserPost(user.$id, contentId); // Call delete API for post
-                setPosts((prevPosts) =>
-                  prevPosts.filter((post) => post.$id !== contentId)
-                );
+
+                alert(`${type} deleted successfully`);
+              } catch (error) {
+                console.error(`Error deleting ${type}:`, error);
+                alert(`Failed to delete ${type}. Please try again.`);
               }
-              await refetchFunction(); // Call the passed refetch function
-              alert(`${type} deleted successfully`);
             },
           },
         ]
       );
     } catch (error) {
+      console.error("Error in deleteContent:", error);
       alert(error.message);
     }
   };
 
-  //function for fetch all posts
-  const fetchUserPostList = async () => {
-    try {
-      const postList = await getUserPosts(user.$id);
-      setUserPosts(postList);
-    } catch (error) {
-      console.error("Error fetching user posts:", error.message);
-      throw new Error(error.message || "Failed to fetch user post list");
+  //function for fetch user content
+  const fetchUserContents = async (userId, type = null) => {
+    if (!user) {
+      console.error("User is not logged in");
+      return;
     }
-  };
-  //function for fetch all videos
-  const fetchUserVideoList = async () => {
     try {
-      const videoList = await getUserVideos(user.$id);
-      setUserVideos(videoList);
+      let videos = [];
+      let posts = [];
+      // Fetch video content
+      if (!type || type === "video") {
+        videos = await getUserVideos(userId);
+      }
+      // Fetch post content
+      if (!type || type === "post") {
+        posts = await getUserPosts(userId);
+      }
+      // Combine and return content
+      const combinedContent = [
+        ...videos.map((item) => ({ ...item, type: "video" })),
+        ...posts.map((item) => ({ ...item, type: "post" })),
+      ].sort((a, b) => new Date(b.$createdAt) - new Date(a.$createdAt));
+
+      return combinedContent;
     } catch (error) {
-      console.error("Error fetching user videos:", error.message);
-      throw new Error(error.message || "Failed to fetch user video list");
+      console.error(
+        "Error fetching user content:",
+        error.response || error.message
+      );
+      throw new Error(error.message || "Failed to fetch user content list");
     }
   };
 
   //function for fetch SearchVideos
   const fetchSearchVideos = async (query) => {
     if (!query.trim()) {
-      // Handle empty query gracefully, for example, reset search results
+      // Handle empty query,  reset search results
       setSearchVideoResult([]);
       return;
     }
@@ -328,41 +348,6 @@ const GlobalProvider = ({ children }) => {
     }
   };
 
-  //function for user liked videos
-  const fetchUserLikedVideos = async () => {
-    if (!user) return []; // Ensure user is logged in
-    try {
-      // Returns the list of liked video objects
-      const likedVideosList = await getUserLikedVideos(user.$id);
-
-      // Update state with the list of liked video IDs
-      setSavedVideoId(likedVideosList.map((video) => video.$id));
-
-      // Update state with the detailed liked video list
-      setLikedVideoList(likedVideosList);
-
-      return likedVideosList;
-    } catch (error) {
-      console.error("Error fetching liked videos:", error.message);
-      return [];
-    }
-  };
-
-  //function for user liked posts
-  const fetchUserLikedPosts = async () => {
-    if (!user) return []; // Ensure user is logged in
-    try {
-      const likedPostList = await getUserLikedPosts(user.$id); // Fetch videos
-      setSavedPostId(likedPostList.map((post) => post.$id)); // Store IDs
-      setLikedPostList(likedPostList);
-
-      return likedPostList;
-    } catch (error) {
-      console.error("Error fetching liked post:", error.message);
-      return [];
-    }
-  };
-
   //function for user logout
   const userLogout = async () => {
     await logout();
@@ -375,42 +360,45 @@ const GlobalProvider = ({ children }) => {
       value={{
         isLoggedIn,
         setIsLoggedIn,
+        userLogout,
         user,
         setUser,
         isLoading,
-        toggleLikeVideo,
-        toggleLikePost,
+        setRefreshing,
+
+        //? function
         updateContent,
-        videos,
-        posts,
-        latestPosts,
-        userPosts,
-        userVideos,
-        fetchAllVideoContent,
-        fetchAllPostContent,
-        fetchLatestVideoContent,
         deleteContent,
-        fetchUserPostList,
-        fetchUserVideoList,
-        userLogout,
-        fetchUserLikedVideos,
-        fetchUserLikedPosts,
-        likedVideoList,
-        likedPostList,
-        savedVideoId,
-        savedPostId,
-        commonRefresh,
-        refreshing,
+        toggleSaveContent,
         handleCreatePost,
         handleCreatVideo,
+        refreshing,
+
+        //?fetching function
+        fetchUserSavedContent,
+        fetchUserContents,
+        fetchAllContents,
+        fetchLatestVideoContent,
+
         fetchSearchVideos,
         fetchSearchPosts,
         searchPostResult,
         searchVideoResult,
-        menuState,
-        handleToggleMenu,
-        setMenuState,
-        setUserPosts,
+
+        //?Arrays
+        userContents,
+        setUserContents,
+        ////
+        allContents,
+        setAllContents,
+        ////
+        savedContents,
+        setSavedContents,
+        savedContentId,
+        setSavedContentId,
+        ////
+        latestPosts,
+        ////
       }}
     >
       {children}
